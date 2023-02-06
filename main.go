00c5cd4c79edd44ef11d2681dc1a904e9b414261
shanedoc <shanedoc@163.com>
2023-02-06 22:01:38 +0800

    集成 gin

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("hello world")
+	//实例化gin实例
+	r := gin.New()
+	//中间件
+	r.Use(gin.Logger(), gin.Recovery())
+	//注册路由
+	r.GET("/", func(c *gin.Context) {
+		//json格式相应
+		c.JSON(http.StatusOK, gin.H{
+			"hello": "world",
+		})
+	})
+	//处理404
+	r.NoRoute(func(c *gin.Context) {
+		//获取标头信息
+		acceptString := c.Request.Header.Get("Accept")
+		if strings.Contains(acceptString, "text/html") {
+			c.String(http.StatusNotFound, "页面返回404")
+		} else {
+			c.JSON(http.StatusNotFound, gin.H{
+				"error_code":    404,
+				"error_message": "路由未定义，请确认 url 和请求方法是否正确。",
+			})
+		}
+
+	})
+	r.Run(":8000")
+
 }
