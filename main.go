c8cb6a12815384df33871a27654c9df658cc4a14
shanedoc <shanedoc@163.com>
2023-02-24 14:44:53 +0800

    初始化数据库

--- main.go
+++ main.go
@@ -29,6 +29,11 @@ func main() {
 	//注册路由
 	bootstrap.SetupRoute(r)
 
+	//初始化db
+	bootstrap.SetupDB()
+	//初始化路由绑定
+	bootstrap.SetupRoute(r)
+
 	err := r.Run(":8001")
 	if err != nil {
 		// 错误处理，端口被占用了或者其他错误
---
+++ pkg/database/database.go
@@ -0,0 +1,33 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	"gorm.io/gorm"
+	gormlogger "gorm.io/gorm/logger"
+)
+
+//数据库操作
+var DB *gorm.DB
+var SQLDB *sql.DB
+
+//连接数据库
+func Connect(dbConfig gorm.Dialector, _logger gormlogger.Interface) {
+	//gorm.Open连接
+	var err error
+	DB, err := gorm.Open(dbConfig, &gorm.Config{
+		Logger: _logger,
+	})
+	// 处理错误
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	// 获取底层的 sqlDB
+	SQLDB, err = DB.DB()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+}
